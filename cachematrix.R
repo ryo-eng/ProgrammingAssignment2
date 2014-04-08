0baf5b145f8442fffd56a8e71c9bfe25f51eff94
Ryo <ryo-eng@users.noreply.github.com>
2014-04-09 04:44:44 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean) }
 
 
 ## Write a short comment describing this function
+## Totally cannot understand the use of these functions as
+##   it unable print a matrix to proof system.time() faster than apply matrix() and solve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        solve(x) }
